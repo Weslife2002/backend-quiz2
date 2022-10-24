c5857f38bd66a56edd29c8b105fe05531eecb014
Weslife2002 <truongtranduytan@gmail.com>
2022-10-24 17:00:16 +0700

    Approach try to minimize the usage of npm library.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const moment = require('moment-timezone');
+import moment from 'moment-timezone';
 
 let findStartEndOfMonth = (inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
   // Convert UTC back to the current date of the current TimeZone
-  date = new Date(inputDate.getTime() + utcOffset*60*1000);
+  let date = new Date(inputDate.getTime() + utcOffset*60*1000);
   let startDate, endDate;
   startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime());
   endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth() + 1, 1))).getTime() -1);
@@ -18,7 +18,7 @@ console.log(findStartEndOfMonth(new Date("2022-03-01"), 'America/New_York'))
 let findMonthToDate = ((inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
   // Convert UTC back to the current date of the current TimeZone
-  date = new Date(inputDate.getTime() + utcOffset*60*1000);
+  let date = new Date(inputDate.getTime() + utcOffset*60*1000);
   let startDate, endDate;
   startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime());
   endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 1))).getTime() -1);
@@ -33,7 +33,7 @@ console.log(findMonthToDate(new Date("2022-12-24T20:00:00"), 'Asia/Ho_Chi_Minh')
 let findLastWeekPeriod = ((inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
   // Convert UTC back to the current date of the current TimeZone
-  date = new Date(inputDate.getTime() + utcOffset*60*1000);
+  let date = new Date(inputDate.getTime() + utcOffset*60*1000);
   let day = (date.getUTCDay() ? date.getUTCDay(): 7);
   let startOfWeek = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()))).getTime() - (6 + day)*24*60*60*1000);
   let endOfWeek = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()))).getTime() - (day-1)*24*60*60*1000 -1);
@@ -43,4 +43,4 @@ let findLastWeekPeriod = ((inputDate, tzString) => {
   return { startOfWeek, endOfWeek }
 })
 
-console.log(findLastWeekPeriod(new Date("2022-10-23T16:00:00Z"), 'Asia/Ho_Chi_Minh'))
+console.log(findLastWeekPeriod(new Date("2022-10-23T19:00:00Z"), 'Asia/Ho_Chi_Minh'))
