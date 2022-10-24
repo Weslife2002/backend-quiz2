146d6f9e27a424b43abc47cd05ff51ecab4bd3ab
Weslife2002 <truongtranduytan@gmail.com>
2022-10-24 16:03:25 +0700

    Second commit

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const moment = require('moment-timezone');
 let findStartEndOfMonth = (inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
   // Convert UTC back to the current date of the current TimeZone
-  date = new Date(inputDate.getTime() + utcOffset*60*1000);
+  date = new Date(inputDate.getTime() + utcOffset * 60 * 1000);
   let startDate, endDate;
-  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime() - utcOffset*60*1000);
-  endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth() + 1, 1))).getTime() -1 - utcOffset*60*1000);
+  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime() - utcOffset * 60 * 1000);
+  endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth() + 1, 1))).getTime() - 1 - utcOffset * 60 * 1000);
   // Convert it back to the current date of the current TimeZone
   startDate = new Date(startDate.getTime());
   endDate = new Date(endDate.getTime());
   // return date.toLocaleString("en-US", {timeZone: tzString});
-  return {startDate, endDate}
+  return { startDate, endDate }
 }
 
 console.log(findStartEndOfMonth(new Date("2022-03-01"), 'America/New_York'))
@@ -19,10 +19,10 @@ console.log(findStartEndOfMonth(new Date("2022-03-01"), 'America/New_York'))
 let findMonthToDate = ((inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
   // Convert UTC back to the current date of the current TimeZone
-  date = new Date(inputDate.getTime() + utcOffset*60*1000);
+  date = new Date(inputDate.getTime() + utcOffset * 60 * 1000);
   let startDate, endDate;
-  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime() - utcOffset*60*1000);
-  endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 1))).getTime() -1 - utcOffset*60*1000);
+  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime() - utcOffset * 60 * 1000);
+  endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 1))).getTime() - 1 - utcOffset * 60 * 1000);
   // Convert it back to the current date of the current TimeZone
   startDate = new Date(startDate.getTime());
   endDate = new Date(endDate.getTime());
@@ -33,7 +33,7 @@ let findMonthToDate = ((inputDate, tzString) => {
 console.log(findMonthToDate(new Date("2022-12-24T20:00:00"), 'Asia/Ho_Chi_Minh'))
 
 let findLastWeekPeriod = ((date, tzString) => {
-  let currentDate = moment(new Date(date.getTime() - 7*24*60*60*1000));
+  let currentDate = moment(new Date(date.getTime() - 7 * 24 * 60 * 60 * 1000));
   let startOfWeek = currentDate.tz(tzString).startOf('isoWeek').toDate();
   let endOfWeek = currentDate.tz(tzString).endOf('isoWeek').toDate();
   return { startOfWeek, endOfWeek }
