a281bee539e27dc4d973cad2c33820ebeff9e5b4
Weslife2002 <truongtranduytan@gmail.com>
2022-10-24 16:40:24 +0700

    Third commit

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const moment = require('moment-timezone');
 let findStartEndOfMonth = (inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
   // Convert UTC back to the current date of the current TimeZone
-  date = new Date(inputDate.getTime() + utcOffset * 60 * 1000);
+  date = new Date(inputDate.getTime() + utcOffset*60*1000);
   let startDate, endDate;
-  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime() - utcOffset * 60 * 1000);
-  endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth() + 1, 1))).getTime() - 1 - utcOffset * 60 * 1000);
+  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime());
+  endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth() + 1, 1))).getTime() -1);
   // Convert it back to the current date of the current TimeZone
-  startDate = new Date(startDate.getTime());
-  endDate = new Date(endDate.getTime());
-  // return date.toLocaleString("en-US", {timeZone: tzString});
-  return { startDate, endDate }
+  startDate = new Date(startDate.getTime() - utcOffset*60*1000);
+  endDate = new Date(endDate.getTime() - utcOffset*60*1000);
+  return {startDate, endDate}
 }
 
 console.log(findStartEndOfMonth(new Date("2022-03-01"), 'America/New_York'))
@@ -19,24 +18,29 @@ console.log(findStartEndOfMonth(new Date("2022-03-01"), 'America/New_York'))
 let findMonthToDate = ((inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
   // Convert UTC back to the current date of the current TimeZone
-  date = new Date(inputDate.getTime() + utcOffset * 60 * 1000);
+  date = new Date(inputDate.getTime() + utcOffset*60*1000);
   let startDate, endDate;
-  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime() - utcOffset * 60 * 1000);
-  endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 1))).getTime() - 1 - utcOffset * 60 * 1000);
+  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime());
+  endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 1))).getTime() -1);
   // Convert it back to the current date of the current TimeZone
-  startDate = new Date(startDate.getTime());
-  endDate = new Date(endDate.getTime());
-  // return date.toLocaleString("en-US", {timeZone: tzString});
+  startDate = new Date(startDate.getTime() - utcOffset*60*1000);
+  endDate = new Date(endDate.getTime() - utcOffset*60*1000);
   return { startDate, endDate }
 })
 
 console.log(findMonthToDate(new Date("2022-12-24T20:00:00"), 'Asia/Ho_Chi_Minh'))
 
-let findLastWeekPeriod = ((date, tzString) => {
-  let currentDate = moment(new Date(date.getTime() - 7 * 24 * 60 * 60 * 1000));
-  let startOfWeek = currentDate.tz(tzString).startOf('isoWeek').toDate();
-  let endOfWeek = currentDate.tz(tzString).endOf('isoWeek').toDate();
+let findLastWeekPeriod = ((inputDate, tzString) => {
+  let utcOffset = moment.tz(tzString).utcOffset();
+  // Convert UTC back to the current date of the current TimeZone
+  date = new Date(inputDate.getTime() + utcOffset*60*1000);
+  let day = (date.getUTCDay() ? date.getUTCDay(): 7);
+  let startOfWeek = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()))).getTime() - (6 + day)*24*60*60*1000);
+  let endOfWeek = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()))).getTime() - (day-1)*24*60*60*1000 -1);
+  // Convert it back to the current date of the current TimeZone
+  startOfWeek = new Date(startOfWeek.getTime() - utcOffset*60*1000);
+  endOfWeek = new Date(endOfWeek.getTime() - utcOffset*60*1000);
   return { startOfWeek, endOfWeek }
 })
 
-console.log(findLastWeekPeriod(new Date("2022-10-23T17:00:00Z"), 'Asia/Ho_Chi_Minh'))
+console.log(findLastWeekPeriod(new Date("2022-10-23T16:00:00Z"), 'Asia/Ho_Chi_Minh'))
