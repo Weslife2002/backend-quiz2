3fc7c9b19adb0d3ee15f493ae5f1a37ed066a911
Weslife2002 <truongtranduytan@gmail.com>
2022-10-24 17:09:09 +0700

    Approach try to minimize the usage of npm library.

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import moment from 'moment-timezone';
 
 let findStartEndOfMonth = (inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
-  // Convert UTC back to the current date of the current TimeZone
+  // Convert input time to the current date of the current TimeZone
   let date = new Date(inputDate.getTime() + utcOffset * 60 * 1000);
   let startDate, endDate;
-  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime());
+  startDate = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1));
   endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth() + 1, 1))).getTime() - 1);
   // Convert it back to the current date of the current TimeZone
   startDate = new Date(startDate.getTime() - utcOffset * 60 * 1000);
@@ -17,10 +17,10 @@ console.log(findStartEndOfMonth(new Date("2022-03-01"), 'America/New_York'))
 
 let findMonthToDate = ((inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
-  // Convert UTC back to the current date of the current TimeZone
+  // Convert input time to the current date of the current TimeZone
   let date = new Date(inputDate.getTime() + utcOffset * 60 * 1000);
   let startDate, endDate;
-  startDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))).getTime());
+  startDate = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1));
   endDate = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 1))).getTime() - 1);
   // Convert it back to the current date of the current TimeZone
   startDate = new Date(startDate.getTime() - utcOffset * 60 * 1000);
@@ -32,7 +32,7 @@ console.log(findMonthToDate(new Date("2022-12-24T20:00:00"), 'Asia/Ho_Chi_Minh')
 
 let findLastWeekPeriod = ((inputDate, tzString) => {
   let utcOffset = moment.tz(tzString).utcOffset();
-  // Convert UTC back to the current date of the current TimeZone
+  // Convert input time back to the current date of the current TimeZone
   let date = new Date(inputDate.getTime() + utcOffset * 60 * 1000);
   let day = (date.getUTCDay() ? date.getUTCDay() : 7);
   let startOfWeek = new Date((new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()))).getTime() - (6 + day) * 24 * 60 * 60 * 1000);
