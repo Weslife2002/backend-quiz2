01eda03422a83c7d33150761cce6f25da561f108
Weslife2002 <truongtranduytan@gmail.com>
2022-10-24 15:52:59 +0700

    second commit

---
+++ subMain.js
@@ -0,0 +1,26 @@
+const moment = require('moment-timezone');
+
+let findStartEndOfMonth = (date, tzString) => {
+  startDate = moment(date).tz(tzString).startOf('month').toDate();
+  endDate = moment(date).tz(tzString).endOf('month').toDate();
+  return { startDate, endDate }
+}
+
+console.log(findStartEndOfMonth(new Date("2022-03-01"), 'America/New_York'))
+
+let findMonthToDate = ((date, tzString) => {
+  startDate = moment(date).tz(tzString).startOf('month').toDate();
+  endDate = moment(date).tz(tzString).endOf('day').toDate();
+  return { startDate, endDate }
+})
+
+console.log(findMonthToDate(new Date("2022-12-24T20:00:00"), 'Asia/Ho_Chi_Minh'))
+
+let findLastWeekPeriod = ((date, tzString) => {
+  let currentDate = moment(new Date(date.getTime() - 7 * 24 * 60 * 60 * 1000));
+  let startOfWeek = currentDate.tz(tzString).startOf('isoWeek').toDate();
+  let endOfWeek = currentDate.tz(tzString).endOf('isoWeek').toDate();
+  return { startOfWeek, endOfWeek }
+})
+
+console.log(findLastWeekPeriod(new Date("2022-10-23T17:00:00Z"), 'Asia/Ho_Chi_Minh'))
